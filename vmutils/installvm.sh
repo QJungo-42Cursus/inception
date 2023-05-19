efad8aff0c58e278ed1575e2d7d7c018d00c9a25
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-19 08:58:54 +0200

    Update installvm.sh

--- vmutils/installvm.sh
+++ vmutils/installvm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export VM_PATH="${HOME}/my_vm_created_by_cli";
-export VM_NAME="vmCreatedByCLI"
 export VDI_FILE="${VM_PATH}/Archlinux_64.vdi"
 export ARCHISO_FILE="${VM_PATH}/archlinux_bootable.iso"
 export ARCHISO_VERSION="2023.03.01"
