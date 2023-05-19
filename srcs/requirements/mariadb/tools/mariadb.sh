0a4e62e30a2c7291b0d1de551bc3ccee8638b0bb
Quentin Jungo <quentin.jungo@gmail.com>
2023-05-19 06:50:59 +0200

    fleeeex

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -6,46 +6,45 @@ mysql_install_db
 
 if [ ! -d "/var/lib/mysql/$MYSQL_DATABASE" ] ; then
 
-# Set root option so that connexion without root password is not possible
-mysql_secure_installation << _EOF_
+# # Set root option so that connexion without root password is not possible
+# mysql_secure_installation << _EOF_
 
-Y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-Y
-n
-Y
-Y
-_EOF_
-# empty password
-# set root password ?
-# the new password
-# confirm the new password
-# remove anonymous users ?
-# disallow root login remotely ?
-# remove test database and access to it ?
-# reload privilege tables now ?
+# Y
+# $MYSQL_ROOT_PASSWORD
+# $MYSQL_ROOT_PASSWORD
+# Y
+# n
+# Y
+# Y
+# _EOF_
+# # empty password
+# # set root password ?
+# # the new password
+# # confirm the new password
+# # remove anonymous users ?
+# # disallow root login remotely ?
+# # remove test database and access to it ?
+# # reload privilege tables now ?
 
-#Add a root user on 127.0.0.1 to allow remote connexion 
-#Flush privileges allow to your sql tables to be updated automatically when you modify it
-mysql --user=root launch mysql command line client
-mysql --user=root << _EOF_
-GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-FLUSH PRIVILEGES;
-_EOF_
+# #Add a root user on 127.0.0.1 to allow remote connexion 
+# #Flush privileges allow to your sql tables to be updated automatically when you modify it
+# mysql --user=root launch mysql command line client
+# mysql --user=root << _EOF_
+# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+# FLUSH PRIVILEGES;
+# _EOF_
 
 # Create database and user in the database for wordpress
 mysql --user=root << _EOF_
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
-GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 FLUSH PRIVILEGES;
+ALTER USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
 _EOF_
-
-# Import database in the mysql command line
-# mysql --user=root -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
 
 fi
 
 /etc/init.d/mysql stop
 
-exec "$@"
+mysqld --user=root --console --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/create-wp.sh
+++ srcs/requirements/wordpress/tools/create-wp.sh
@@ -9,11 +9,12 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	rm -rf latest.tar.gz
 	rm -rf wordpress
 
-	sed -i "s/username_here/$MYSQL_USER/g" wp-config-sample.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g" wp-config-sample.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g" wp-config-sample.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config-sample.php
 	cp wp-config-sample.php wp-config.php
+	sed -i "s/username_here/$MYSQL_USER/g"          wp-config.php
+	sed -i "s/password_here/$MYSQL_PASSWORD/g"      wp-config.php
+	sed -i "s/localhost/$MYSQL_HOSTNAME/g"          wp-config.php
+	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
+	
 fi
 
 php-fpm7.3 -F;
