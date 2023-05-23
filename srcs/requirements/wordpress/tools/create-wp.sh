0f95dd8f11a87c785f2e415027b7c8de947d4c0a
Quentin Jungo <43724937+JungoQuentin@users.noreply.github.com>
2023-05-23 07:56:10 +0200

    wordusers

--- srcs/requirements/wordpress/tools/create-wp.sh
+++ srcs/requirements/wordpress/tools/create-wp.sh
@@ -1,19 +1,46 @@
 #!/bin/sh
 
+# while true ; do
+# 	sleep 1
+# done
+
+# rm -rf /var/www/html/*
+
+# while ! mysqladmin ping -h"${MYSQL_HOSTNAME}" --silent; do
+# 	sleep 1
+# done
+
 if [ ! -f /var/www/html/wp-config.php ]; then
 	cd /var/www/html
 	rm index.html
-	wget http://wordpress.org/latest.tar.gz
-	tar xfz latest.tar.gz
-	mv wordpress/* .
-	rm -rf latest.tar.gz
-	rm -rf wordpress
 
-	cp wp-config-sample.php wp-config.php
-	sed -i "s/username_here/$MYSQL_USER/g"          wp-config.php
-	sed -i "s/password_here/$MYSQL_PASSWORD/g"      wp-config.php
-	sed -i "s/localhost/$MYSQL_HOSTNAME/g"          wp-config.php
-	sed -i "s/database_name_here/$MYSQL_DATABASE/g" wp-config.php
+	wp core download \
+	--locale="en_US" \
+	--allow-root
+
+	wp core config \
+	--dbname="${MYSQL_DATABASE}" \
+	--dbuser="${MYSQL_USER}" \
+	--dbhost="${MYSQL_HOSTNAME}" \
+	--dbpass="${MYSQL_PASSWORD}" \
+	--allow-root
+	
+	chmod 600 wp-config.php
+
+	wp core install \
+	--url=qjungo.42.fr \
+	--title="Example" \
+	--admin_user="${WORDPRESS_ADMIN}" \
+	--admin_password="${WORDPRESS_PASSWORD}" \
+	--admin_email="${WORDPRESS_MAIL}" \
+	--allow-root
+
+	# wp user create \
+	# "${WORDPRESS_USER}" \
+	# "${WORDPRESS_USER_MAIL}" \
+	# --role="author" \
+	# --user_pass="${WORDPRESS_USER_PASSWORD}" \
+	# --allow-root
 fi
 
 php-fpm7.3 -F;
