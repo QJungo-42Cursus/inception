d67e4eae9573927608289c99bb4e359f242c5b47
Quentin Jungo <qjungo@c1r10s1.42lausanne.ch>
2023-05-23 11:45:04 +0200

    simplify-wordpress

---
+++ srcs/requirements/wordpress/create-wp.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+while ! mysqladmin ping -h"${MYSQL_HOSTNAME}" --silent; do
+	sleep 1
+done
+
+rm -rf /var/www/html/*
+
+cd /var/www/html
+rm index.html
+
+wp core download \
+--locale="en_US" \
+--allow-root
+
+wp core config \
+--dbname="${MYSQL_DATABASE}" \
+--dbuser="${MYSQL_USER}" \
+--dbhost="${MYSQL_HOSTNAME}" \
+--dbpass="${MYSQL_PASSWORD}" \
+--allow-root
+
+chmod 777 wp-config.php
+
+wp core install \
+--url=qjungo.42.fr \
+--title="Rust" \
+--admin_user="${WORDPRESS_ADMIN}" \
+--admin_password="${WORDPRESS_PASSWORD}" \
+--admin_email="${WORDPRESS_MAIL}" \
+--allow-root
+
+wp user create \
+"${WORDPRESS_USER}" \
+"${WORDPRESS_USER_MAIL}" \
+--role="author" \
+--user_pass="${WORDPRESS_USER_PASSWORD}" \
+--allow-root
+
+php-fpm7.3 -F;
